dac7afc2990ae9a32c1fc1df6e77272f4f48ba04
tom <tom.carton@gmail.com>
2017-12-09 20:45:25 +0100

    Fixed memory leaks and issues

--- 2017/Day06/6.c
+++ 2017/Day06/6.c
@@ -9,76 +9,86 @@
 #include <stdbool.h>
 
 
-int bank[] = { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6, };
 // int bank[] = { 0, 2, 7, 0, };
-int bankCount;
+int Bank[] = { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6, };
+int BankCount;
 
 void dumpBank(int *bank) {
-    for (unsigned int i = 0; i < bankCount; ++i) {
-        printf("%d\t", bank[i]);
+    for (unsigned int i = 0; i < BankCount; ++i) {
+        printf("%d\t", Bank[i]);
     }
     printf("\n");
 }
 
-int *history = NULL;
-int historyCount = 0;
+int *History = NULL;
+int HistorySpace = 0;
+int HistoryCount = 0;
 
 void saveHistory(int *bank) {
-    if (history == NULL) {
-        history = malloc(sizeof(bank) * 256);
+    if (History == NULL) {
+        HistorySpace = 8;
+        History = malloc(sizeof(Bank) * HistorySpace);
     }
-
-    memcpy(history + historyCount * bankCount, bank, sizeof(int) * bankCount);
-    ++historyCount;
+    if (HistoryCount + 1 >= HistorySpace) {
+        int *h = malloc(2 * HistorySpace * sizeof(Bank));
+        memcpy(h, History, HistorySpace * sizeof(Bank));
+        free(History);
+        History = h;
+        HistorySpace *= 2;
+    }
+    
+    memcpy(History + HistoryCount * BankCount, bank, sizeof(Bank));
+    ++HistoryCount;
 }
 
 bool isHistory(int *bank) {
-    for (unsigned int i = 0; i < historyCount; ++i) {
-        int *savedBank = &history[i * bankCount];
-        if (memcmp(bank, savedBank, sizeof(int) * bankCount) == 0) {
-        	printf("loop count = %d\n", historyCount - i);
+    for (unsigned int i = 0; i < HistoryCount; ++i) {
+        int *savedBank = &History[i * BankCount];
+        if (memcmp(bank, savedBank, sizeof(Bank)) == 0) {
+            printf("loop count = %d\n", HistoryCount - i);
             return true;
         }
     }
-
+    
     return false;
 }
 
 void dispatchBank(int *bank) {
     int max = 0;
     unsigned int maxi = 0;
-    for (unsigned int i = 0; i < bankCount; ++i) {
+    for (unsigned int i = 0; i < BankCount; ++i) {
         if (bank[i] > max) {
             max = bank[i];
             maxi = i;
         }
     }
-
+    
     bank[maxi] = 0;
     unsigned int i = maxi + 1;
     while (max--) {
-        i %= bankCount;
-
+        i %= BankCount;
+        
         ++bank[i++];
     }
 }
 
 int main(int argc, const char * argv[]) {
-
-    bankCount = sizeof(bank) / sizeof(bank[0]);
-	int iter = 0;
-   	
-   	printf("%d:\t", iter++);
-	dumpBank(bank);
+    
+    BankCount = sizeof(Bank) / sizeof(Bank[0]);
+    int iter = 0;
+    
+    printf("%d:\t", iter++);
+    dumpBank(Bank);
     
     do {
-        saveHistory(bank);
-        dispatchBank(bank);
-
+        saveHistory(Bank);
+        dispatchBank(Bank);
+        
         printf("%d:\t", iter++);
-        dumpBank(bank);
-    } while (isHistory(bank) == false);
-
+        dumpBank(Bank);
+    } while (isHistory(Bank) == false);
+    
+    free(History);
+    
     return 0;
 }
-
