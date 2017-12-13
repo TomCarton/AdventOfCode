a24fc697ca160a93e7e21cd09c3beb3d7a99eb64
Thomas CARTON <thomas@madebykawet.com>
2017-12-13 15:02:15 +0100

    Some cleaning and part two in progress

--- 2017/Day13/scanner.c
+++ 2017/Day13/scanner.c
@@ -9,6 +9,8 @@
 
 #include "input.h"
 
+#define kIncrement "   "
+
 typedef struct {
 	unsigned int depth;
 	unsigned int range;
@@ -19,8 +21,7 @@ unsigned int LayerCount = 0;
 
 unsigned int MaxDepth = 0;
 
-unsigned int Scanner = 0;
-unsigned int Packet = 0;
+unsigned int Scanner, Packet;
 
 unsigned int parse(const char *buffer, unsigned int size, LAYER *layer) {
 	char *s = (char *)buffer, *start = NULL;
@@ -72,9 +73,7 @@ unsigned int scannerPosition(unsigned int index, unsigned int range) {
 	return pos;
 }
 
-void dumpLayers(unsigned int depth) {
-	printf("\n");
-
+void dumpLayers(unsigned int depth, bool show) {
 	if (depth > 0) {
 		depth = MaxDepth < depth ? MaxDepth : depth;
 	} else {
@@ -83,6 +82,7 @@ void dumpLayers(unsigned int depth) {
 	++depth;
 
 	// 0:  0   1   2   3   4   5   6   7   8  ...
+	printf(kIncrement);
 	for (unsigned int i = 0; i < depth; ++i) {
 		printf("%2d  ", i);
 	}
@@ -92,20 +92,22 @@ void dumpLayers(unsigned int depth) {
 	for (unsigned int d = 0; d < 5; ++d) {
 		LAYER *layer = Layer;
 
+		printf(kIncrement);
+		
 		for (unsigned int l = 0; l < depth; ++l) {
 			if (l > layer->depth) {
 				++layer;
 			}
 			if (l < layer->depth) {
 				if (d == 0) {
-					printf(d == 0 && l == Packet ? "(.) " : "... ");
+					printf(show && d == 0 && l == Packet ? "(.) " : "... ");
 				} else {					
 					printf("    ");
 				}
 			} else if (scannerPosition(Scanner, layer->range) == d) {
-				printf(d == 0 && l == Packet ? "(S) " : "[S] ");
+				printf(show && d == 0 && l == Packet ? "(S) " : "[S] ");
 			} else if (d < layer->range) {
-				printf(d == 0 && l == Packet ? "( ) " : "[ ] ");
+				printf(show && d == 0 && l == Packet ? "( ) " : "[ ] ");
 			} else {
 				printf("    ");
 			}
@@ -115,45 +117,71 @@ void dumpLayers(unsigned int depth) {
 	}
 }
 
-int main() {
-
-	char *data = NULL;
-	unsigned int read = readInput("input.txt", &data);
-
-	unsigned int length = parse(data, read, NULL);
-
-	Layer = malloc(length * sizeof(LAYER));
-	parse(data, read, Layer);
-
-	free(data);
-
+bool runPacket(unsigned int delay, unsigned int picosecond, bool display) {
 	unsigned int severity = 0;
+
+	Scanner = delay;
+	Packet = 0;
+
+	if (display) {
+		printf("\n=================================================================++\n");
+		printf(" Run with delay %d\n", delay);
+		printf("===================================================================\n\n");
+
+		printf(kIncrement "Initial state:\n");
+		dumpLayers(8, false);
+
+	}
+
 	for (unsigned int k = 0; k < MaxDepth + 1; ++k) {
-		printf("Picosecond %d:\n", k);
-		bool caught = false;
+		if (display) {
+			printf(kIncrement "Picosecond %d:\n", k + delay);
 
-		dumpLayers(8);
+			dumpLayers(8, true);
+		}
 
-		for (unsigned int i = 0; i < length; ++i) {
+		for (unsigned int i = 0; i < LayerCount; ++i) {
 			if (Packet != Layer[i].depth) continue;
 
-			if (scannerPosition(Scanner, Layer[i].range) == 0) {
-				printf("### Caught! severity += %d * %d\n", Layer[i].depth, Layer[i].range);
+			if (i != 0 && scannerPosition(Scanner, Layer[i].range) == 0) {
 				severity += Layer[i].depth * Layer[i].range;
 			}
 		}
 
 		++Scanner;
-		dumpLayers(8);
+		if (display) {
+			dumpLayers(8, true);
+			printf("\n");
+		}
 
 		++Packet;
+	}
 
-		printf("\n");
+	if (display && severity > 0) printf("### CAUGHT! ### Severity = %d ###\n\n", severity);
+
+	return severity > 0;
+}
+
+int main() {
+
+	char *data = NULL;
+	unsigned int read = readInput("input.txt", &data);
+
+	LayerCount = parse(data, read, NULL);
+	Layer = malloc(LayerCount * sizeof(LAYER));
+	parse(data, read, Layer);
+
+	free(data);
+
+	unsigned int d = 0; {
+	// for (unsigned int d = 0; d < 100; ++d) {
+		if (runPacket(d, 6, true) == false) {
+			printf("should delay %d picosecond%s...\n", d, d > 1 ? "s" : "");
+			// break;
+		}
 	}
 
 	free(Layer);
-
-	printf("### Severity = %d\n", severity);
 
 	return 0;
 }
