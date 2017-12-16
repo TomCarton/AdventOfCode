cdde5130d5ee6fb0fd207c101415ae1e860f7590
tom <tom.carton@gmail.com>
2017-12-16 15:54:46 +0100

    In progress

--- 2017/Day16/main.c
+++ 2017/Day16/main.c
@@ -5,45 +5,42 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "../common/input.h"
 
 static const unsigned int kDancerCount = 16;
+static const unsigned int kDanceCount = 1000000000;
 
-int main() {
+void dance(char *dancer, const char *steps, unsigned int size, bool stats) {
+	char temp[kDancerCount + 1];
 
-	char dancer[kDancerCount + 1];
+	unsigned int sCount = 0;
+	unsigned int xCount = 0;
+	unsigned int pCount = 0;
 
-	for (unsigned int i = 0; i < kDancerCount; ++i) {
-		dancer[i] = 'a' + i;
-	}
-	dancer[kDancerCount] = '\0';
-
-	char *dance = NULL;
-	unsigned int size = readInput("input.txt", &dance);
-
-	char c, *s = dance;
+	char c, *s = (char *)steps;
 	char step[6], *st = step;
 
-	while (s - dance <= size) {
+	while (s - steps <= size) {
 		c = *s++;
- 		if (s - dance <= size && c != ',') {
+
+ 		if (s - steps <= size && c != ',') {
 			*st++ = c;
 		} else {
 			*st = '\0';
 			st = step;
 
 			// perform dance step
-			printf("%10s", step);
-
 			switch (step[0]) {
 
 				case 's': {
+					++sCount;
+
 					unsigned int sp;
 					sscanf(&step[1], "%d", &sp);
 
-					char temp[kDancerCount + 1];
 					strcpy(temp, dancer);
 					for (unsigned int i = 0; i < kDancerCount; ++i) {
 						dancer[i] = i < sp ? temp[kDancerCount - sp + i] : temp[i - sp];
@@ -53,6 +50,8 @@ int main() {
 				}
 
 				case 'x': {
+					++xCount;
+
 					unsigned int d1, d2;
 					sscanf(&step[1], "%d/%d", &d1, &d2);
 
@@ -64,6 +63,8 @@ int main() {
 				}
 
 				case 'p': {
+					++pCount;
+
 					char p1, p2;
 					sscanf(&step[1], "%c/%c", &p1, &p2);
 
@@ -80,12 +81,59 @@ int main() {
 					break;
 				}
 			}
-
-			printf("-- %s\n", dancer);
 		}
 	}
 
+	if (stats) {
+		printf ("s: %d, x: %d, p: %d\n", sCount, xCount, pCount);
+	}
+}
+
+void move(char *dancer, const char *plan) {
+	char temp[kDancerCount];
+	strcpy(temp, dancer);
+
+	for (unsigned int i = 0; i < kDancerCount; ++i) {
+		dancer[i] = temp[plan[i] - 'a'];
+	}
+}
+
+void prepare(char *dancers) {
+	for (unsigned int i = 0; i < kDancerCount; ++i) {
+		dancers[i] = 'a' + i;
+	}
+	dancers[kDancerCount] = '\0';
+}
+
+int main() {
+
+	char *steps = NULL;
+	unsigned int size = readInput("input.txt", &steps);
+
+
+	// dance plan
+	char plan[kDancerCount + 1];
+	prepare(plan);
+	dance(plan, steps, size, true);
+
+	printf("\ndance plan: '%s'\n", plan);
+
+	// get period
+	unsigned int period = 42;
+
+	// perform dance
+	char dancer[kDancerCount + 1];
+	prepare(dancer);
+
+	unsigned int remain = kDanceCount % period;
+
+	for (unsigned int j = 0; j < remain; ++j) {
+		dance(dancer, steps, size, false);
+	}
+
 	printf("\ndancers: '%s'\n", dancer);
+
+	free(steps);
 
 	return 0;
 }
