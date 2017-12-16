616d4e02c64b9e5342228a0138948263fcb260ad
tom <tom.carton@gmail.com>
2017-12-16 08:51:59 +0100

    Started Day16

---
+++ 2017/Day16/main.c
@@ -0,0 +1,17 @@
+// Day16 - dance
+//
+// written by Thomas CARTON
+//
+
+#include <stdlib.h>
+
+#include "../common/input.h"
+
+
+int main() {
+
+	char *input = NULL;
+	unsigned int size = readInput("input.txt", &input);
+	
+	return 0;
+}
