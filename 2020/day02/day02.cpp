320e593673c8f64c72b0239637251341fc894690
Thomas CARTON <tom.carton@gmail.com>
2020-12-02 06:21:04 +0100

    Solved day 02

---
+++ 2020/day02/day02.cpp
@@ -0,0 +1,44 @@
+// day02.cpp
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    unsigned int countP1 = 0, countP2 = 0;
+
+    std::ifstream file("input.txt");
+
+    unsigned char chars[26];
+    std::string line;
+    while (std::getline(file, line))
+    {
+        memset(chars, 0, sizeof(chars));
+
+        size_t delim1 = line.find('-');
+        size_t delim2 = line.find(':');
+
+        char letter = line[delim2 - 1];
+        unsigned int min = atoi(line.substr(0, delim1).c_str());
+        unsigned int max = atoi(line.substr(delim1 + 1, delim2).c_str());
+        const char *password = line.c_str() + delim2 + 2;
+
+        for (unsigned int i = 0; i < strlen(password); ++i)
+            ++chars[(password[i] | ' ') - 'a'];
+
+        unsigned int occurences = chars[letter - 'a'];
+
+        if (occurences >= min && occurences <= max)
+            ++countP1;
+
+        if ((password[min - 1] == letter) ^ (password[max - 1] == letter))
+            ++countP2;
+   }
+
+    std::cout << "Part One: " << countP1 << '\n';
+    std::cout << "Part Two: " << countP2 << '\n';
+
+    return 0;
+}
