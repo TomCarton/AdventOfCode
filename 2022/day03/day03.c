1abe3709695dbb1c949c61e96096ded7458c3c22
Thomas CARTON <tom.carton@gmail.com>
2022-12-04 00:17:07 +0100

    Day03: C - unnested wip

--- 2022/day03/day03.c
+++ 2022/day03/day03.c
@@ -5,10 +5,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 static const char kDefaultFilename[] = "input.txt";
 static const unsigned int kMaxBufferSize = 64;
 
+void delay(int ms)
+{
+    clock_t endTime = clock() + 1000 * ms;
+    while (clock() < endTime);
+}
+
+void dumpCursors(const char *line, int c1, int c2)
+{
+    printf("\033[H\033[J");
+
+    printf("  ");
+    for (unsigned i = 0; i < c1; ++i)
+        printf(" ");
+    printf("v\n");
+
+    printf("  %s\n  ", line);
+
+    for (unsigned i = 0; i < c2; ++i)
+        printf(" ");
+    printf("^\n");
+
+    // delay(10);
+}
 
 int main(int argc, char *argv[])
 {
@@ -36,28 +60,50 @@ int main(int argc, char *argv[])
     char *line, *p1, *p2, c1, c2;
     while((line = fgets(buffer, kMaxBufferSize, file)) != NULL)
     {
-        unsigned int half = (unsigned int)strlen(line) >> 1;
+        unsigned int len = (unsigned int)strlen(line) - 1;
+        line[len] = line[len] == '\n' ?: '\0';
+        unsigned int half = len >> 1;
 
         p1 = line;
-        for (unsigned int i = 0; i < half; ++i)
+        p2 = line + half;
+
+        do
         {
-            c1 = *p1++;
+            dumpCursors(line, p1 - line, p2 - line);
 
-            p2 = line + half;
-            for (unsigned int j = 0; j < half; ++j)
-            {
-                c2 = *p2++;
-
-                if (c1 == c2)
-                    break;
-            }
-
-            if (c1 == c2)
+            char vis = *p2;
+            if ((c1 = *p1) == *p2++)
             {
                 sum += c1 - 38 - (~c1 & 32 ? 0 : 58);
                 break;
             }
-        }
+
+            char c = 1 - !!(*(p2 + 1));
+            p2 -= c * half;
+            p1 += c;
+       }
+        while (p1 - line < half);
+
+        // p1 = line;
+        // for (unsigned int i = 0; i < half; ++i)
+        // {
+        //     c1 = *p1++;
+
+        //     p2 = line + half;
+        //     for (unsigned int j = 0; j < half; ++j)
+        //     {
+        //         c2 = *p2++;
+
+        //         if (c1 == c2)
+        //             break;
+        //     }
+
+        //     if (c1 == c2)
+        //     {
+        //         sum += c1 - 38 - (~c1 & 32 ? 0 : 58);
+        //         break;
+        //     }
+        // }
     }
 
     printf("Part One: %d\n", sum);
